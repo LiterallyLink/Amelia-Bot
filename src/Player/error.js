a412897dafab443ea7b8740c0aa3a3885c1ece4a
LinkWasTaken <zechariahpeoples@gmail.com>
2021-08-15 16:11:24 -0700

    Update error.js

--- src/Player/error.js
+++ src/Player/error.js
@@ -1,3 +1,1 @@
-module.exports = (client, queue, error) => {
-	console.log(`[${queue.guild.name}] Error emitted from the queue: ${error.message}`);
-};
+module.exports = (client, queue, error) => console.log(`[${queue.guild.name}] Error emitted from the queue: ${error.message}`);
