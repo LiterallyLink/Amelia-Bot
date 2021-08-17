523d8ea36779ed60d10c8782122101c9acefaaef
LinkWasTaken <zechariahpeoples@gmail.com>
2021-08-16 19:44:40 -0700

    Created AI command

---
+++ src/Commands/Fun/ai.js
@@ -0,0 +1,35 @@
+const request = require('node-superfetch');
+const Command = require('../../Structures/Command');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			description: 'Provides a link to invite the bot to your guild',
+			category: 'Fun'
+		});
+	}
+
+	async run(message, args) {
+		const baseUrl = `https://api.monkedev.com/fun/chat`;
+
+		if (!args) {
+			const emptyResponses = ['Huh? At least try to say something!', 'Got nothing to say?'];
+			const emptyMessageResponse = Math.floor(Math.random() * emptyResponses.length);
+			return message.reply({ content: `${emptyMessageResponse}` });
+		}
+
+		try {
+			const { body } = await request.get(`${baseUrl}?msg=${encodeURIComponent(args.join(' '))}&uid=${message.author.id}`);
+
+			if (!body) {
+				return message.channel.send(`I'm sorry, could you say that again?`);
+			}
+
+			return message.reply(body.response);
+		} catch (err) {
+			return console.log(err);
+		}
+	}
+
+};
