39e65751fc71bd50fbde6cdb30dae53e9da48cc9
LinkWasTaken <zechariahpeoples@gmail.com>
2021-08-19 23:30:43 -0700

    Updated functions
    Optimized functions and removed blacklist functions to work on later.

--- src/Structures/Utilities/Database.js
+++ src/Structures/Utilities/Database.js
@@ -1,7 +1,5 @@
-/* eslint-disable consistent-return */
 const GuildSchema = require('../../Models/guildSchema');
-const custom = require('../../Models/customCommand');
-const profile = require('../../Models/profileSchema');
+const Profile = require('../../Models/profileSchema');
 
 module.exports = class Database {
 
@@ -10,6 +8,7 @@ module.exports = class Database {
 	}
 
 
+	// Fetches the prefix for guild and DMs
 	async getPrefix(message) {
 		let clientPrefix = this.client.prefix;
 
@@ -20,6 +19,23 @@ module.exports = class Database {
 		if (settings) clientPrefix = settings.prefix;
 
 		return clientPrefix;
+	}
+
+	async fetchUser(userID, guildID) {
+		const userProfile = await Profile.findOne({ userId: userID, guildId: guildID });
+
+		if (!userProfile) await this.createUserProfile(userID, guildID);
+
+		return userProfile;
+	}
+
+	async createUserProfile(userID, guildID) {
+		const newUser = new Profile({
+			userId: userID,
+			guildId: guildID
+		});
+
+		await newUser.save().catch(err => console.log(`Failed to create user: ${err}`));
 	}
 
 	async fetchGuild(guild) {
@@ -41,48 +57,6 @@ module.exports = class Database {
 		newGuild.save()
 			.then(result => console.log(result))
 			.catch(error => console.error(error));
-	}
-
-	async customCommand(message, prefix) {
-		if (!message.guild) return;
-
-		custom.findOne({
-			guildID: message.guild.id,
-			Command: message.content.slice(prefix.length)
-		},
-		(err, data) => {
-			if (err) throw err;
-			if (data) return message.channel.send(data.Content);
-		});
-	}
-
-	async isUserBlacklisted(message) {
-		const user = await this.fetchUser(message.guild.id, message.author.id);
-		console.log(user);
-
-		return false;
-	}
-
-	async fetchBlacklisted() {
-		const blacklisted = await profile.find({ isBlacklisted: true });
-		const blacklistedUsers = blacklisted.map(user => `<@${user.userId}> - ${user.userId}`);
-		return blacklistedUsers;
-	}
-
-	async blacklist(message, user, boolean) {
-		const blacklisted = await this.fetchUser(message.guild.id, user.id);
-		if (blacklisted.isBlacklisted === boolean) {
-			const booleanState = boolean ? 'already' : 'not';
-			return message.reply(`This user is ${booleanState} blacklisted`)
-				.then(msg => msg.delete({ timeout: 10000 }));
-		}
-		blacklisted.isBlacklisted = boolean;
-		await blacklisted.save().catch(err => console.log(`Failed to blacklist ${user} ${err}`));
-	}
-
-	async fetchUser(guildID, userID) {
-		const userProfile = await profile.findOne({ guildId: guildID, userId: userID });
-		return userProfile;
 	}
 
 };
