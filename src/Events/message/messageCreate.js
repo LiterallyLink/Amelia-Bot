1b3d06d1dc5f714f61720da5452fb42d0754d5fe
LinkWasTaken <zechariahpeoples@gmail.com>
2021-08-26 00:17:54 -0700

    Update messageCreate.js

--- src/Events/message/messageCreate.js
+++ src/Events/message/messageCreate.js
@@ -6,6 +6,7 @@ module.exports = class extends Event {
 
 	async run(message) {
 		if (message.author.bot) return;
+
 		const { user, commands, aliases, utils, database, level, embed, defaultPerms } = this.client;
 
 		const prefixRegexp = RegExp(`^<@!?${user.id}> `);
