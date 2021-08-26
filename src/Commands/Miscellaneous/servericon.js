7696658dc66f46fdd4b0df131626bf786ed744b3
LinkWasTaken <zechariahpeoples@gmail.com>
2021-08-26 00:20:21 -0700

    Created servericon

---
+++ src/Commands/Miscellaneous/servericon.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require('discord.js');
+const Command = require('../../Structures/Command');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			description: 'Provides the picture of the guild.',
+			category: 'Miscellaneous',
+			guildOnly: true
+		});
+	}
+
+	async run(message) {
+		const guildIconEmbed = new MessageEmbed()
+			.setTitle(`${message.guild.name} Guild Icon`)
+			.setDescription(`[Guild Icon URL](${message.guild.iconURL()})`)
+			.setImage(message.guild.iconURL())
+			.setColor(this.client.embed.color.default);
+		return message.channel.send({ embeds: [guildIconEmbed] });
+	}
+
+};
