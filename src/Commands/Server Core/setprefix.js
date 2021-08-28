a7acd5ef0c262da9c7dd030080faddd662666f5e
LinkWasTaken <zechariahpeoples@gmail.com>
2021-08-28 12:27:42 -0700

    Changed categories name

--- src/Commands/Server Core/setprefix.js
+++ src/Commands/Server Core/setprefix.js
@@ -7,7 +7,7 @@ module.exports = class extends Command {
 	constructor(...args) {
 		super(...args, {
 			description: "Set's a new server prefix",
-			category: 'Server Core',
+			category: 'Settings',
 			usage: '(new prefix)',
 			userPerms: ['ADMINISTRATOR'],
 			guildOnly: true
