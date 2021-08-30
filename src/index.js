a164d436fbb966a44b7d3e66a0ddd20848604d68
LinkWasTaken <zechariahpeoples@gmail.com>
2021-08-29 23:55:20 -0700

    added ytdlOptions to player

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+/* eslint-disable no-bitwise */
 const AmeliaClient = require('./Structures/AmeliaClient');
 const { Player } = require('discord-player');
 const config = require('../config.json');
 
 const client = new AmeliaClient(config);
-client.player = new Player(client);
+client.player = new Player(client, { ytdlOptions: {
+	filter: 'audioonly',
+	highWaterMark: 1 << 25
+} });
 
 client.start();
