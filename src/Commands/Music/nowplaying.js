42ef064b8afd91d91fe897b199f485fe9d592175
LinkWasTaken <zechariahpeoples@gmail.com>
2021-11-02 15:15:27 -0600

    Update nowplaying.js
    Added voice check

--- src/Commands/Music/nowplaying.js
+++ src/Commands/Music/nowplaying.js
@@ -10,12 +10,12 @@ module.exports = class extends Command {
 			aliases: ['np', 'current'],
 			description: 'Provides the current song playing',
 			category: 'Music',
-			guildOnly: true
+			guildOnly: true,
+			voiceChannelOnly: true
 		});
 	}
 
 	async run(message) {
-		if (!this.client.music.isInChannel(message)) return;
 		if (!this.client.music.canModifyQueue(message)) return;
 
 		const { player, embed, user } = this.client;
