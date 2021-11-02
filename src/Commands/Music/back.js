b4d72f741e5f6dc4bd5c26e357b2ffa66f452fe8
LinkWasTaken <zechariahpeoples@gmail.com>
2021-11-02 15:01:08 -0600

    Updated back
    Added voice check and proper checks

--- src/Commands/Music/back.js
+++ src/Commands/Music/back.js
@@ -9,19 +9,19 @@ module.exports = class extends Command {
 			aliases: ['previous'],
 			description: 'Plays the previous song',
 			category: 'Music',
-			guildOnly: true
+			guildOnly: true,
+			voiceChannelOnly: true
 		});
 	}
 
 	async run(message) {
-		if (!this.client.music.isInChannel(message)) return;
 		if (!this.client.music.canModifyQueue(message)) return;
 
 		const { player, embed } = this.client;
 
 		const queue = player.getQueue(message.guild.id);
 
-		if (!queue && !queue.playing) {
+		if (!queue || !queue.playing || !queue.previousTracks[1]) {
 			const noQueue = new MessageEmbed()
 				.setDescription('There is no previous song to play')
 				.setColor(embed.color.default);
