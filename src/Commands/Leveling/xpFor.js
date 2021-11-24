49ba29aaf9077e5e5536efa844e3516ff6d8dc63
LinkWasTaken <zechariahpeoples@gmail.com>
2021-11-24 03:06:15 -0600

    Update xpFor.js
    Update with emojis?

--- src/Commands/Leveling/xpFor.js
+++ src/Commands/Leveling/xpFor.js
@@ -12,35 +12,34 @@ module.exports = class extends Command {
 		});
 	}
 
-	async run(message, [number], prefix) {
-		if (Number.isInteger(parseInt(number)) && number >= 0) {
-			const guildData = await this.client.database.fetchGuild(message.guild);
-			const user = await this.client.database.fetchUser(message.author.id, message.guild.id);
-
-			const { xpSettings } = guildData;
-			const { maxXPGain, minXPGain } = xpSettings;
-			const xpToLevel = this.client.level.xpFor(number);
-			const xpDifference = xpToLevel - user.xp;
-			const xpMedian = Math.round((maxXPGain + minXPGain) / 2);
-			const averageXPFromCurrentLevel = Math.round(xpDifference / xpMedian);
-
-			const xpForEmbed = new MessageEmbed()
-				.setTitle(`XP Calculations for level ${number}`)
-				.setDescription(`Every minute, you earn ${xpSettings.minXPGain}-${xpSettings.maxXPGain} XP from chatting!`)
-				.addField(`From Level 0 to ${number}`, `**Total XP needed to reach level ${number}:** ${xpToLevel}`)
-				.addField(`Estimated messages needed:`, `${Math.round(xpToLevel / xpMedian)} average, ${Math.round(xpToLevel / maxXPGain)} min, ${Math.round(xpToLevel / minXPGain)} max.`)
-				.addField(`From your current level to ${number}`, `**Total XP needed to reach level ${number}:** ${xpDifference}`)
-				.addField(`Estimated messages needed:`, `${averageXPFromCurrentLevel} average, ${Math.round(xpDifference / maxXPGain)} min, ${Math.round(xpDifference / minXPGain)} max.`)
-				.setColor(this.client.embed.color.default);
-			return message.channel.send({ embeds: [xpForEmbed] });
+	async run(message, [number]) {
+		if (this.client.utils.isInt(number)) {
+			const noNumberGivenEmbed = new MessageEmbed()
+				.setAuthor(`Invalid Level Provided`, message.author.displayAvatarURL())
+				.setDescription(`Please provide a valid number\n\nUsage: \`${this.usage}\``)
+				.setThumbnail(this.client.embed.thumbnails.ameShake)
+				.setColor(this.client.embed.color.error);
+			return message.channel.send({ embeds: [noNumberGivenEmbed] });
 		}
 
-		const noNumberGivenEmbed = new MessageEmbed()
-			.setAuthor(`Invalid Level Provided`, message.author.displayAvatarURL())
-			.setDescription(`Please provide a valid number\n\nUsage: \`${prefix}${this.usage}\``)
-			.setThumbnail(this.client.embed.thumbnails.ameShake)
-			.setColor(this.client.embed.color.error);
-		return message.channel.send({ embeds: [noNumberGivenEmbed] });
+		const guildData = await this.client.database.fetchGuild(message.guild);
+		const user = await this.client.database.fetchUser(message.author.id, message.guild.id);
+
+		const { maxXPGain, minXPGain } = guildData.xpSettings;
+		const xpToLevel = this.client.level.xpFor(number);
+		const xpDifference = xpToLevel - user.xp;
+		const xpMedian = Math.round((maxXPGain + minXPGain) / 2);
+		const averageXPFromCurrentLevel = Math.round(xpDifference / xpMedian);
+
+		const xpForEmbed = new MessageEmbed()
+			.setTitle(`XP Calculations for level ${number}`)
+			.setDescription(`Every minute, you earn ${minXPGain}-${maxXPGain} XP from chatting!`)
+			.addField(`From Level 0 to ${number}`, `**Total XP needed to reach level ${number}:** ${xpToLevel}`)
+			.addField(`Estimated messages needed:`, `${Math.round(xpToLevel / xpMedian)} average, ${Math.round(xpToLevel / maxXPGain)} min, ${Math.round(xpToLevel / minXPGain)} max.`)
+			.addField(`From your current level to ${number}`, `**Total XP needed to reach level ${number}:** ${xpDifference}`)
+			.addField(`Estimated messages needed:`, `${averageXPFromCurrentLevel} average, ${Math.round(xpDifference / maxXPGain)} min, ${Math.round(xpDifference / minXPGain)} max.`)
+			.setColor(this.client.embed.color.default);
+		return message.channel.send({ embeds: [xpForEmbed] });
 	}
 
 };
